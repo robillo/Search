29fe92938514a3da4f2379ca9aa76fafabe54dc9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-11 17:21:13 +0530

    Data

--- app/src/main/java/com/appbusters/robinkamboj/search/model/Data.java
+++ app/src/main/java/com/appbusters/robinkamboj/search/model/Data.java
@@ -17,4 +17,24 @@ public class Data {
     public String getText() {
         return text;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Data data = (Data) o;
+
+        if (id != data.id) return false;
+        return text != null ? text.equals(data.text) : data.text == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        return result;
+    }
+
 }
