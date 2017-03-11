bbe79992e7233acbf58b93903248b2cfbb177b24
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-11 16:55:15 +0530

    MVC

--- app/src/main/java/com/appbusters/robinkamboj/search/view/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/search/view/MainActivity.java
@@ -1,10 +1,12 @@
-package com.appbusters.robinkamboj.search;
+package com.appbusters.robinkamboj.search.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.appbusters.robinkamboj.search.R;
 
 public class MainActivity extends AppCompatActivity {
 
